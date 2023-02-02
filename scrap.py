437c820f4d5d466323af6d7ea15a92c818b6bdf9
Alexandre Delaistre <alexandre.quilan.delaistre@gmail.com>
2023-02-02 16:46:01 +0100

    preparing to poo

--- scrap.py
+++ scrap.py
@@ -2,33 +2,71 @@
 # ensure you have pip installer
 import requests 
 from bs4 import BeautifulSoup 
-
+import time
 # L'url du site que je souhaite Scraper
 baseUrl = 'https://www.studyrama.com'
-uri = "/formations/annuaire-des-formations?btnRechercherFormations=rechercher&metier=20039043"
+uri = "/formations/annuaire-des-formations?btnRechercherFormations=rechercher&metier=20039043&page="
 
-#Instanciation de mon proxy
-response = requests.get(baseUrl + uri )
 
+#Genere des liens avec l'argument "page" qui s'incrémente
+def getLinks(url, nbPg):
+    # initialisation du resultat (vide pour l'instant)
+    urls = []
+    # Pour chaque page
+    for i in range(nbPg):
+        # Ajoutes la concatenation de l'url avec l'index au tableau d'urls
+        urls.append(url + str(i))
+    return urls
 
 
 #fonction qui permet de "crawler" sur mon site et recuperer tous les liens sur la page visée
-def process(swoup):
+def getEndpoints(soup):
+    time.sleep(1)
     #ATTENTION, la suite de cette fonction ne marche que pour mon site, c'est un exemple
     #l'exercice etant de refaire une fonction pour VOTRE site a scraper
-    ul = swoup.find('ul', { "class": "trackingContainer"})
+    ul = soup.find('ul', { "class": "trackingContainer"})
     lis = ul.findAll('li')
+    links = []
     for li in lis:
         a = li.find('a')
-        try:
-            print(baseUrl + a['href'])
-            requests.get(baseUrl + a['href'])
+        try: 
+            links.append(a['href'])
         except:
-            pass  
+            print(li)
+            print('ERROR: No link')
+    return links
 
-#si mon site renvoie un code HTTP 200 (OK)
-if response.ok:
-    #je passe le contenue html de ma page dans un "parser"
-    swoupGang = BeautifulSoup(response.text,'html.parser')
-    # execute mon parser dessus pour récuperer mes liens
+def swoup(url, process):
+    #Instanciation de mon proxy
+    response = requests.get(url)
+    #si mon site renvoie un code HTTP 200 (OK)
+    if response.ok:
+        #je passe le contenue html de ma page dans un "parser"
+        soup = BeautifulSoup(response.text, 'html.parser')
+        try:
+            #Je retourne l'execution de ma fonction process prenan ma SWOUP SWOUP en parametre
+            return process(soup)
+        except Exception:
+            print("ERROR: Impossible to process ! " )
+    else:
+        print("ERROR: Failed Connect")
+    return 
+
+#concatene mes liens a l'url
+def addBaseUrl(baseUrl, urls):
+    res = []
+    for url in urls:
+        res.append(baseUrl + url)
+    return res
+
+
+#Execution
+urls = []
+for link in getLinks(baseUrl + uri, 4):
+    print("Checking " + link)
+    urls.extend(addBaseUrl(baseUrl, swoup(link, getEndpoints)))
+        
+print(urls)
+
+
   
